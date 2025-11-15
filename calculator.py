4fa6031ed4486fcf2d31172c0d8bc9e5a97a4da6
Ethan Ferrara <emferrara2006@gmail.com>
2025-11-15 15:28:22 -0500

    Fixed

--- calculator.py
+++ calculator.py
@@ -17,9 +17,9 @@ def add(a, b): return a + b
 
 def subtract(a, b): return a - b
 
-def multiply(a, b): return a * b
+def mul(a, b): return a * b
 
-def divide(a,b):
+def div(a,b):
 	if a == 0:
 		raise ZeroDivisionError("Cannot divide by zero")
 	else:
@@ -35,4 +35,4 @@ def logarithm(a, b):
 	else:
 		return math.log(b, a)
 
-def exponent(a, b): return a**b
+def exp(a, b): return a**b
